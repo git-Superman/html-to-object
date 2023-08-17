62da76844ffa1d958bf5a60e5b1937386fcf0e3c
gitsuperman <316053557@qq.com>
2023-08-17 18:03:34 +0800

    add browser

---
+++ browser.js
@@ -0,0 +1,95 @@
+;(function (factory) {
+    
+    window.htmlToObject = factory(jQuery);
+
+})(function(jQuery) {
+
+    function strIntoObjects(parser) {
+        let type = parser.match(/\/\w+\(/)[0];
+        let fns = parser.split(type);
+    
+        let target = fns[0];
+        let fn2 = fns[1];
+        
+        fn2 = fn2.slice(0, -1);
+    
+        const methods = {
+            '/text(': () => ({ target, type: 'text' }),
+            '/html(': () => ({ target, type: 'html' }),
+            '/attr(': () => ({ target, type: 'attr', attr: fn2 }),
+            '/each(': () => ({ target, type: 'each', list: fn2.split(',').map(item => strIntoObjects((item).trim())) })
+        }
+    
+        return methods[type] ? methods[type]() : null
+    }
+    
+    
+    
+    // to objects
+    function strAllIntoObjects(parser) {
+        let result = {};
+        Object.keys(parser).forEach(key => {
+            result[key] = strIntoObjects(parser[key]);
+        });
+    
+        return result;
+    }
+    
+    
+    
+    // format text
+    function formatText(parent, parser) {
+        const self = parser.target === 'self' ? parent : parent.find(parser.target);
+        switch (parser.type) {
+            case 'text':
+                return self.text().trim();
+            case 'html':
+                return (self.html() || '').trim();
+            case 'attr':
+                return (self.attr(parser.attr) || '').trim();
+            default:
+                return '';
+        }
+    }
+    
+    
+    /**
+     * @param {String} html
+     * @param {Object} parser
+     * @returns {Object}
+     * 
+     */
+    function htmlToObject(html, parser) {
+    
+        if( typeof parser !== 'object' ) throw Error('argument parser It\'s an object')
+    
+        const $ = jQuery(html || '');
+        const parser_match = strAllIntoObjects(parser);
+        const result = {}
+        
+    
+        Object.keys(parser_match).forEach(key => {
+            let item = parser_match[key];
+    
+            if( item )
+            {
+                if( item.type == 'each' ) {
+                    let list = [];
+                    $.find(item.target).each((i, item2) => {
+                        list.push(item.list.map(self => formatText( jQuery(item2), self)));
+                    });
+                    result[key] = list;
+                }
+                else {
+                    result[key] = formatText($, item);
+                }
+            }
+        });
+    
+    
+        return result;
+    }
+
+
+    return htmlToObject;
+});
--- index.js
+++ index.js
@@ -1,102 +1,92 @@
 const cheerio = require('cheerio');
 
 
-( function( global, factory ) {
-	"use strict";
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		module.exports = factory();
-	} else {
-		throw Error();
-	}
-} )( typeof window !== "undefined" ? window : this, function() {
 
-    
-    function strIntoObjects(parser) {
-        let type = parser.match(/\/\w+\(/)[0];
-        let fns = parser.split(type);
-    
-        let target = fns[0];
-        let fn2 = fns[1];
-        
-        fn2 = fn2.slice(0, -1);
-    
-        const methods = {
-            '/text(': () => ({ target, type: 'text' }),
-            '/html(': () => ({ target, type: 'html' }),
-            '/attr(': () => ({ target, type: 'attr', attr: fn2 }),
-            '/each(': () => ({ target, type: 'each', list: fn2.split(',').map(item => strIntoObjects((item).trim())) })
-        }
-    
-        return methods[type] ? methods[type]() : null
-    }
-    
-    
-    
-    // to objects
-    function strAllIntoObjects(parser) {
-        let result = {};
-        Object.keys(parser).forEach(key => {
-            result[key] = strIntoObjects(parser[key]);
-        });
-    
-        return result;
-    }
-    
-    
-    
-    // format text
-    function formatText(parent, parser) {
-        const self = parser.target === 'self' ? parent : parent.find(parser.target);
-        switch (parser.type) {
-            case 'text':
-                return self.text().trim();
-            case 'html':
-                return (self.html() || '').trim();
-            case 'attr':
-                return (self.attr(parser.attr) || '').trim();
-            default:
-                return '';
-        }
-    }
-    
-    
-    /**
-     * @param {String} html
-     * @param {Object} parser
-     * @returns {Object}
-     * 
-     */
-    function htmlToObject(html, parser) {
+function strIntoObjects(parser) {
+    let type = parser.match(/\/\w+\(/)[0];
+    let fns = parser.split(type);
 
-        if( typeof parser !== 'object' ) throw Error('argument parser It\'s an object')
+    let target = fns[0];
+    let fn2 = fns[1];
     
-        const $ = cheerio.load(html || '');
-        const parser_match = strAllIntoObjects(parser);
-        const result = {}
-        
+    fn2 = fn2.slice(0, -1);
+
+    const methods = {
+        '/text(': () => ({ target, type: 'text' }),
+        '/html(': () => ({ target, type: 'html' }),
+        '/attr(': () => ({ target, type: 'attr', attr: fn2 }),
+        '/each(': () => ({ target, type: 'each', list: fn2.split(',').map(item => strIntoObjects((item).trim())) })
+    }
+
+    return methods[type] ? methods[type]() : null
+}
+
+
+
+// to objects
+function strAllIntoObjects(parser) {
+    let result = {};
+    Object.keys(parser).forEach(key => {
+        result[key] = strIntoObjects(parser[key]);
+    });
+
+    return result;
+}
+
+
+
+// format text
+function formatText(parent, parser) {
+    const self = parser.target === 'self' ? parent : parent.find(parser.target);
+    switch (parser.type) {
+        case 'text':
+            return self.text().trim();
+        case 'html':
+            return (self.html() || '').trim();
+        case 'attr':
+            return (self.attr(parser.attr) || '').trim();
+        default:
+            return '';
+    }
+}
+
+
+/**
+ * @param {String} html
+ * @param {Object} parser
+ * @returns {Object}
+ * 
+ */
+function htmlToObject(html, parser) {
+
+    if( typeof parser !== 'object' ) throw Error('argument parser It\'s an object')
+
+    const $ = cheerio.load(html || '');
+    const parser_match = strAllIntoObjects(parser);
+    const result = {}
     
-        Object.keys(parser_match).forEach(key => {
-            let item = parser_match[key];
-    
-            if( item )
-            {
-                if( item.type == 'each' ) {
-                    let list = [];
-                    $(item.target).each((i, item2) => {
-                        list.push(item.list.map(self => formatText( $(item2), self)));
-                    });
-                    result[key] = list;
-                }
-                else {
-                    result[key] = formatText($('html'), item);
-                }
+
+    Object.keys(parser_match).forEach(key => {
+        let item = parser_match[key];
+
+        if( item )
+        {
+            if( item.type == 'each' ) {
+                let list = [];
+                $(item.target).each((i, item2) => {
+                    list.push(item.list.map(self => formatText( $(item2), self)));
+                });
+                result[key] = list;
             }
-        });
-    
-    
-        return result;
-    }
+            else {
+                result[key] = formatText($('html'), item);
+            }
+        }
+    });
 
 
-    return htmlToObject
-});
+    return result;
+}
+
+
+module.exports = htmlToObject
